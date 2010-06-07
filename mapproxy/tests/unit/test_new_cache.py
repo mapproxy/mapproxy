f6aea22c34faef21533b2c1be99bce964c4acbba
Oliver Tonnhofer <olt@bogosoft.com>
2010-06-07 11:18:58 +0200

    updated tests for new cache

--- mapproxy/tests/unit/test_new_cache.py
+++ mapproxy/tests/unit/test_new_cache.py
@@ -22,6 +22,7 @@ from mapproxy.core.cache import (
     WMSClient,
     ResolutionConditional,
     SRSConditional,
+    MapExtend,
 )
 from mapproxy.core.grid import TileGrid
 from mapproxy.core.srs import SRS
@@ -36,6 +37,8 @@ from collections import defaultdict
 
 from nose.tools import eq_, raises
 
+
+GLOBAL_GEOGRAPHIC_EXTEND = MapExtend((-180, -90, 180, 90), SRS(4326))
 
 tmp_lock_dir = None
 def setup():
@@ -69,15 +72,15 @@ class TestTiledSourceGlobalGeodetic(object):
         self.client = MockTileClient()
         self.source = TiledSource(self.grid, self.client)
     def test_match(self):
-        self.source.get(MapQuery([-180, -90, 0, 90], (256, 256), SRS(4326)))
-        self.source.get(MapQuery([0, -90, 180, 90], (256, 256), SRS(4326)))
+        self.source.get_map(MapQuery([-180, -90, 0, 90], (256, 256), SRS(4326)))
+        self.source.get_map(MapQuery([0, -90, 180, 90], (256, 256), SRS(4326)))
         eq_(self.client.requested_tiles, [(0, 0, 1), (1, 0, 1)])
     @raises(InvalidSourceQuery)
     def test_wrong_size(self):
-        self.source.get(MapQuery([-180, -90, 0, 90], (512, 256), SRS(4326)))
+        self.source.get_map(MapQuery([-180, -90, 0, 90], (512, 256), SRS(4326)))
     @raises(InvalidSourceQuery)
     def test_wrong_srs(self):
-        self.source.get(MapQuery([-180, -90, 0, 90], (512, 256), SRS(4326)))
+        self.source.get_map(MapQuery([-180, -90, 0, 90], (512, 256), SRS(4326)))
 
 
 class MockFileCache(FileCache):
@@ -135,7 +138,7 @@ class MockSource(Source):
         Source.__init__(self, *args)
         self.requested = []
     
-    def get(self, query):
+    def get_map(self, query):
         self.requested.append((query.bbox, query.size, query.srs))
         return ImageSource(create_debug_img(query.size))
 
@@ -157,7 +160,7 @@ class MockWMSClient(object):
     def __init__(self):
         self.requested = []
     
-    def get(self, query):
+    def get_map(self, query):
         self.requested.append((query.bbox, query.size, query.srs))
         return ImageSource(create_debug_img(query.size))
 
@@ -209,9 +212,9 @@ class TestTileManagerWMSSource(object):
 
 class SlowMockSource(MockSource):
     supports_meta_tiles = True
-    def get(self, query):
+    def get_map(self, query):
         time.sleep(0.1)
-        return MockSource.get(self, query)
+        return MockSource.get_map(self, query)
 
 class TestTileManagerLocking(object):
     def setup(self):
@@ -282,7 +285,7 @@ class TestDirectMapLayer(object):
     def setup(self):
         self.client = MockWMSClient()
         self.source = WMSSource(self.client)
-        self.layer = DirectMapLayer(self.source)
+        self.layer = DirectMapLayer(self.source, GLOBAL_GEOGRAPHIC_EXTEND)
     
     def test_get_map(self):
         result = self.layer.get_map(MapQuery((-180, -90, 180, 90), (300, 150), SRS(4326), 'png'))
@@ -302,7 +305,7 @@ class TestDirectMapLayerWithSupportedSRS(object):
     def setup(self):
         self.client = MockWMSClient()
         self.source = WMSSource(self.client)
-        self.layer = DirectMapLayer(self.source)
+        self.layer = DirectMapLayer(self.source, GLOBAL_GEOGRAPHIC_EXTEND)
     
     def test_get_map(self):
         result = self.layer.get_map(MapQuery((-180, -90, 180, 90), (300, 150), SRS(4326), 'png'))
@@ -339,16 +342,16 @@ class TestWMSClient(object):
         self.req_template = WMS111MapRequest(url='http://localhost/service?', param={
             'format': 'image/png', 'layers': 'foo'
         })
-        self.client = WMSClient(self.req_template, self.http_client, [SRS(4326)])
+        self.client = WMSClient(self.req_template, http_client=self.http_client, supported_srs=[SRS(4326)])
         
     def test_get_map(self):
-        result = self.client.get(MapQuery((-180, -90, 180, 90), (300, 150), SRS(4326)))
+        result = self.client.get_map(MapQuery((-180, -90, 180, 90), (300, 150), SRS(4326)))
         assert query_eq(self.http_client.requested[0], "http://localhost/service?"
             "layers=foo&width=300&version=1.1.1&bbox=-180,-90,180,90&service=WMS"
             "&format=image%2Fpng&styles=&srs=EPSG%3A4326&request=GetMap&height=150")
     
     def test_get_map_transformed(self):
-        result = self.client.get(MapQuery(
+        result = self.client.get_map(MapQuery(
            (556597, 4865942, 1669792, 7361866), (300, 150), SRS(900913)))
         assert query_eq(self.http_client.requested[0], "http://localhost/service?"
             "layers=foo&width=300&version=1.1.1"
@@ -364,8 +367,10 @@ class MockLayer(object):
 class TestResolutionConditionalLayers(object):
     def setup(self):
         self.low = MockLayer()
+        self.low.transparent = False #TODO
         self.high = MockLayer()
-        self.layer = ResolutionConditional(self.low, self.high, 10, SRS(900913))
+        self.layer = ResolutionConditional(self.low, self.high, 10, SRS(900913),
+            GLOBAL_GEOGRAPHIC_EXTEND)
     def test_resolution_low(self):
         self.layer.get_map(MapQuery((0, 0, 10000, 10000), (100, 100), SRS(900913)))
         assert self.low.requested
@@ -396,7 +401,7 @@ class TestSRSConditionalLayers(object):
             (self.l4326, (SRS('EPSG:4326'),)), 
             (self.l900913, (SRS('EPSG:900913'), SRS('EPSG:31467'))),
             (self.l32632, (SRSConditional.PROJECTED,)),
-        ])
+        ], GLOBAL_GEOGRAPHIC_EXTEND)
     def test_srs_match(self):
         assert self.layer._select_layer(SRS(4326)) == self.l4326
         assert self.layer._select_layer(SRS(900913)) == self.l900913
