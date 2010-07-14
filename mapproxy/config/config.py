906b7eac8848779ef77971234d52fda2187fdc34
Oliver Tonnhofer <olt@bogosoft.com>
2010-07-14 10:36:28 +0200

    allow to overwrite default axis ordering for broken clients (transplanted from 9c8ac31c8e8ab799acdb3d991bc47854c5dea210)
    --HG--
    extra : transplant_source : %9C%8A%C3%1C%8E%8A%B7%99%AC%DB%3D%99%1B%C4xT%C5%DE%A2%10

--- mapproxy/config/config.py
+++ mapproxy/config/config.py
@@ -93,9 +93,13 @@ def finish_base_config(bc=None):
     bc = bc or base_config()
     if 'srs' in bc:
         # build union of default axis_order_xx_ and the user configured axis_order_xx
-        bc.srs.axis_order_ne = set(bc.srs.axis_order_ne).union(bc.srs.axis_order_ne_)
-        bc.srs.axis_order_en = set(bc.srs.axis_order_en).union(bc.srs.axis_order_en_)
-    
+        default_ne = bc.srs.axis_order_ne_
+        default_en = bc.srs.axis_order_en_
+        # remove from default to allow overwrites
+        default_ne.difference_update(set(bc.srs.axis_order_en))
+        default_en.difference_update(set(bc.srs.axis_order_ne))
+        bc.srs.axis_order_ne = default_ne.union(set(bc.srs.axis_order_ne))
+        bc.srs.axis_order_en = default_en.union(set(bc.srs.axis_order_en))
     if 'wms' in bc:
         bc.wms.srs = set(bc.wms.srs)
 
