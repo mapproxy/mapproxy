b34ab219bbca86f12b9e6aa6da5e62222c0650a0
Oliver Tonnhofer <olt@bogosoft.com>
2011-01-06 13:28:38 +0100

    add support for HTTP X-Forwarded-Proto
    --HG--
    branch : 0.9.1.x

--- mapproxy/request/base.py
+++ mapproxy/request/base.py
@@ -205,14 +205,21 @@ class Request(object):
         elif 'HTTP_HOST' in self.environ:
             return self.environ['HTTP_HOST']
         result = self.environ['SERVER_NAME']
-        if ((self.environ['wsgi.url_scheme'], self.environ['SERVER_PORT'])
+        if ((self.url_scheme, self.environ['SERVER_PORT'])
             not in (('https', '443'), ('http', '80'))):
             result += ':' + self.environ['SERVER_PORT']
         return result
     
     @cached_property
+    def url_scheme(self):
+        scheme = self.environ.get('HTTP_X_FORWARDED_PROTO')
+        if not scheme:
+            scheme = self.environ['wsgi.url_scheme']
+        return scheme
+    
+    @cached_property
     def host_url(self):
-        return '%s://%s/' % (self.environ['wsgi.url_scheme'], self.host)
+        return '%s://%s/' % (self.url_scheme, self.host)
     
     @property
     def script_url(self):
