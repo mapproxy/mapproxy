085ee14b822f3b27c114760736af9919c2166d99
Oliver Tonnhofer <olt@bogosoft.com>
2011-01-17 18:20:13 +0100

    fixed 2.6 compatibilitiy (missing __future__ import)

--- mapproxy/util/async.py
+++ mapproxy/util/async.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import with_statement
+
 MAX_MAP_ASYNC_THREADS = 20
 
 import Queue
