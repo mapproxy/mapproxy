a6379682324714adba548e64650e36c74afe1cbc
Oliver Tonnhofer <olt@bogosoft.com>
2011-01-17 18:00:03 +0100

    renamed thread_pool_size to concurrent_tile_creators

--- mapproxy/cache/tile.py
+++ mapproxy/cache/tile.py
@@ -61,7 +61,7 @@ class TileManager(object):
         self.minimize_meta_requests = minimize_meta_requests
         self._expire_timestamp = None
         self.transparent = self.sources[0].transparent
-        self.thread_pool_size = base_config().cache.concurrent_tile_creators
+        self.concurrent_tile_creators = base_config().cache.concurrent_tile_creators
         
         if meta_buffer or (meta_size and not meta_size == [1, 1]):
             if all(source.supports_meta_tiles for source in sources):
@@ -80,7 +80,7 @@ class TileManager(object):
             # TODO cache eviction
             if self.is_cached(tile):
                 self.cache.load(tile, with_metadata)
-            else:
+            elif tile.coord is not None:
                 uncached_tiles.append(tile)
         
         if uncached_tiles:
@@ -158,7 +158,7 @@ class TileCreator(object):
         return created_tiles
 
     def _create_single_tiles(self, tiles):
-        if self.tile_mgr.thread_pool_size > 1 and len(tiles) > 1:
+        if self.tile_mgr.concurrent_tile_creators > 1 and len(tiles) > 1:
             return self._create_threaded(self._create_single_tile, tiles)
         
         created_tiles = []
@@ -168,7 +168,8 @@ class TileCreator(object):
     
     def _create_threaded(self, create_func, tiles):
         result = []
-        for new_tiles in async.imap(create_func, tiles):
+        async_pool = async.Pool(self.tile_mgr.concurrent_tile_creators)
+        for new_tiles in async_pool.imap(create_func, tiles):
             result.extend(new_tiles)
         return result
     
@@ -214,7 +215,7 @@ class TileCreator(object):
         return merge_images(imgs)
     
     def _create_meta_tiles(self, meta_tiles):
-        if self.tile_mgr.thread_pool_size > 1 and len(meta_tiles) > 1:
+        if self.tile_mgr.concurrent_tile_creators > 1 and len(meta_tiles) > 1:
             return self._create_threaded(self._create_meta_tile, meta_tiles)
         
         created_tiles = []
