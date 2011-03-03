e407f7e7d28ca6f2bc16808792c25b186d1cf52d
Oliver Tonnhofer <olt@bogosoft.com>
2011-03-03 15:19:01 +0100

    fixed Python 2.5 compat

--- mapproxy/test/http.py
+++ mapproxy/test/http.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import with_statement
+
 import threading
 import sys
 import cgi
