db27cca18c873536a3476b25b7c5353133b498a9
Oliver Tonnhofer <olt@bogosoft.com>
2011-03-29 11:13:17 +0200

    add "mapproxy-util create" script
    --HG--
    rename : mapproxy/config_template/paster/etc/mapproxy.yaml => mapproxy/config_template/base_config/mapproxy.yaml
    rename : mapproxy/config_template/paster/etc/seed.yaml => mapproxy/config_template/base_config/seed.yaml

--- mapproxy/script/util.py
+++ mapproxy/script/util.py
@@ -1,7 +1,28 @@
+# This file is part of the MapProxy project.
+# Copyright (C) 2011 Omniscale <http://omniscale.de>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import with_statement
 import os
-import re
 import optparse
+import re
+import shutil
 import sys
+import textwrap
+
+from mapproxy.version import version
 
 def serve_develop_command(args):
     parser = optparse.OptionParser("usage: %prog serve-develop [options] mapproxy.yaml",
@@ -30,8 +51,7 @@ def serve_develop_command(args):
     
     host, port = parse_bind_address(options.address)
     
-    if options.debug and host != 'localhost':
-        import textwrap
+    if options.debug and host not in ('localhost', '127.0.0.1'):
         print textwrap.dedent("""\
         ################# WARNING! ##################
         Running debug mode with non-localhost address
@@ -74,30 +94,171 @@ def parse_bind_address(address, default=('localhost', 8080)):
     return host, port
 
 
+def create_command(args):
+    cmd = CreateCommand(args)
+    cmd.run()
+
+class CreateCommand(object):
+    templates = {
+        'base-config': {},
+        'wsgi-app': {},
+    }
+    
+    APP_TEMPLATE = textwrap.dedent("""\
+    # WSGI module for use with Apache mod_wsgi or gunicorn
+
+    from mapproxy.wsgiapp import make_wsgi_app
+    application = make_wsgi_app('%s')
+    """)
+
+    def __init__(self, args):
+        parser = optparse.OptionParser("usage: %prog create [options] [destination]")
+        parser.add_option("-t", "--template", dest="template",
+            help="Create a configuration from this template.")
+        parser.add_option("-l", "--list-templates", dest="list_templates",
+            action="store_true", default=False,
+            help="List all available configuration templates.")
+        parser.add_option("-f", "--mapproxy-conf", dest="mapproxy_conf",
+            help="Existing MapProxy configuration (required for some templates).")
+        parser.add_option("--force", dest="force", action="store_true",
+            default=False, help="Force operation (e.g. overwrite existing files).")
+
+        self.options, self.args = parser.parse_args(args)
+        self.parser = parser
+
+    def log_error(self, msg, *args):
+        print >>sys.stderr, 'ERROR:', msg % args
+        
+    def run(self):
+        
+        if self.options.list_templates:
+            print_items(self.templates, title="Available templates")
+            sys.exit(1)
+        elif self.options.template:
+            if self.options.template not in self.templates:
+                self.log_error("unknown template " + self.options.template)
+                sys.exit(1)
+            
+            if len(self.args) != 2:
+                self.log_error("template requires destination argument")
+                sys.exit(1)
+                
+            sys.exit(
+                getattr(self, 'template_' + self.options.template.replace('-', '_'))()
+            )
+        else:
+            self.parser.print_help()
+            sys.exit(1)
+
+    @property
+    def mapproxy_conf(self):
+        if not self.options.mapproxy_conf:
+            self.parser.print_help()
+            self.log_error("template requires --mapproxy-conf option")
+            sys.exit(1)
+        return os.path.abspath(self.options.mapproxy_conf)
+    
+    def template_wsgi_app(self):
+        app_filename = self.args[1]
+        if '.' not in os.path.basename(app_filename):
+            app_filename += '.py'
+        mapproxy_conf = self.mapproxy_conf
+        if os.path.exists(app_filename) and not self.options.force:
+            self.log_error("%s already exists, use --force", app_filename)
+            return 1
+
+        print "writing MapProxy app to %s" % (app_filename, )
+        with open(app_filename, 'w') as f:
+            f.write(self.APP_TEMPLATE % (mapproxy_conf, ))
+        
+        return 0
+    
+    def template_base_config(self):
+        outdir = self.args[1]
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        
+        import mapproxy.config_template
+        template_dir = os.path.join(
+            os.path.dirname(mapproxy.config_template.__file__),
+            'base_config')
+        
+        for filename in ('mapproxy.yaml', 'seed.yaml'):
+            to = os.path.join(outdir, filename)
+            from_ = os.path.join(template_dir, filename)
+            if os.path.exists(to) and not self.options.force:
+                self.log_error("%s already exists, use --force", to)
+                return 1
+            print "writing %s" % (to, )
+            shutil.copy(from_, to)
+            
 commands = {
     'serve-develop': {
         'func': serve_develop_command,
-        'help': 'run MapProxy development server'
+        'help': 'Run MapProxy development server.'
+    },
+    'create': {
+        'func': create_command,
+        'help': 'Create example configurations.'
     },
 }
-def print_help():
-    prog = os.path.basename(sys.argv[0])
-    print >>sys.stdout, 'Usage: %s COMMAND\n' % (prog, )
-    print >>sys.stdout, 'Commands:'
-    for cmd, cmd_dict in commands.iteritems():
-        help = cmd_dict.get('help', '')
+
+
+class NonStrictOptionParser(optparse.OptionParser):
+    def _process_args(self, largs, rargs, values):
+        while rargs:
+            arg = rargs[0]
+            # We handle bare "--" explicitly, and bare "-" is handled by the
+            # standard arg handler since the short arg case ensures that the
+            # len of the opt string is greater than 1.
+            try:
+                if arg == "--":
+                    del rargs[0]
+                    return
+                elif arg[0:2] == "--":
+                    # process a single long option (possibly with value(s))
+                    self._process_long_opt(rargs, values)
+                elif arg[:1] == "-" and len(arg) > 1:
+                    # process a cluster of short options (possibly with
+                    # value(s) for the last one only)
+                    self._process_short_opts(rargs, values)
+                elif self.allow_interspersed_args:
+                    largs.append(arg)
+                    del rargs[0]
+                else:
+                    return
+            except optparse.BadOptionError:
+                largs.append(arg)
+    
+
+def print_items(data, title='Commands'):
+    name_len = max(len(name) for name in data)
+    
+    if title:
+        print >>sys.stdout, '%s:' % (title, )
+    for name, item in data.iteritems():
+        help = item.get('help', '')
+        name = ('%%-%ds' % name_len) % name
         if help:
-            help = ': ' + help
-        print >>sys.stdout, '  %s%s' % (cmd, help)
+            help = '  ' + help
+        print >>sys.stdout, '  %s%s' % (name, help)
 
 def main():
-    if len(sys.argv) < 2:
-        print_help()
+    parser = NonStrictOptionParser("usage: %prog COMMAND [options]",
+        version='MapProxy ' + version, add_help_option=False)
+    options, args = parser.parse_args()
+    
+    if len(args) < 1 or args[0] in ('--help', '-h'):
+        parser.print_help()
+        print
+        print_items(commands)
         sys.exit(1)
         
-    command = sys.argv[1]
+    command = args[0]
     if command not in commands:
-        print_help()
+        parser.print_help()
+        print
+        print_items(commands)
         print >>sys.stdout, '\nERROR: unknown command %s' % (command,)
         sys.exit(1)
     
