70ca2ce65b5f9aafced0b5dbd157fc8ac4393759
Oliver Tonnhofer <olt@bogosoft.com>
2011-05-11 11:18:27 +0200

    fixed python 2.5 compat

--- mapproxy/source/mapnik.py
+++ mapproxy/source/mapnik.py
@@ -14,7 +14,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from __future__ import absolute_import
+from __future__ import with_statement, absolute_import
 
 import sys
 from cStringIO import StringIO
