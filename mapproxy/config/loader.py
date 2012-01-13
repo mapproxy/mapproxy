61f46580b008b474c984ca9a5196a2023ddba7b1
Oliver Tonnhofer <olt@bogosoft.com>
2012-01-13 13:28:07 +0100

    change default cache.base_dir to ./cache_dir

--- mapproxy/config/loader.py
+++ mapproxy/config/loader.py
@@ -814,10 +814,6 @@ class CacheConfiguration(ConfigurationBase):
     defaults = {'format': 'image/png', 'grids': ['GLOBAL_MERCATOR']}
     
     def cache_dir(self):
-        if ('cache_dir' not in self.conf 
-            and 'base_dir' not in self.context.globals.conf.get('cache', {})):
-            warnings.warn('globals.cache.base_dir not defined. default value '
-            '(../var/cache_data) will be changed with 1.4.0.', FutureWarning)
         return self.context.globals.get_path('cache_dir', self.conf,
             global_key='cache.base_dir')
         
