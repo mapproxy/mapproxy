dc57c8513994fa6451bde5b42a7f3e06b4dece09
Oliver Tonnhofer <olt@bogosoft.com>
2012-08-14 08:30:25 +0200

    fixed test

--- mapproxy/test/system/test_util_wms_capabilities.py
+++ mapproxy/test/system/test_util_wms_capabilities.py
@@ -1,12 +1,12 @@
 # This file is part of the MapProxy project.
 # Copyright (C) 2010 Omniscale <http://omniscale.de>
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -61,7 +61,7 @@ class TestUtilWMSCapabilities(object):
         with capture_out() as (out,err):
             assert_raises(SystemExit, wms_capabilities_command, self.args)
         assert err.getvalue().startswith("ERROR:")
-        
+
     def test_request_not_parsable(self):
         with mock_httpd(TESTSERVER_ADDRESS, [({'path': '/', 'method': 'GET'},
                                               {'status': '200', 'body': ''})]):
@@ -89,7 +89,8 @@ class TestUtilWMSCapabilities(object):
                                                   {'status': '200', 'body': capabilities_doc})]):
                 with capture_out() as (out,err):
                     wms_capabilities_command(self.args)
-                assert out.getvalue().startswith('Root-Layer:')
+                lines = out.getvalue().split('\n')
+                assert lines[1].startswith('Root-Layer:')
 
     def test_key_error(self):
         self.args = ['command_dummy', '--host', TESTSERVER_URL + '/service?request=GetCapabilities']
