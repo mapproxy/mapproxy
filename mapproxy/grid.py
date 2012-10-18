mapproxy/mapproxy

commit 7e58eeef8316b6c2c6cf55ad0e0f259ae5218476

improved tile origin handling of WMTS

mapproxy/grid.py
@@ -428,7 +428,9 @@ class TileGrid(object):
         return (x, self.grid_sizes[z][1]-1-y, z)
 
     def supports_access_with_origin(self, origin):
-        if (origin or 'sw') == (self.origin or 'sw'):
+        if origin in (None, 'sw', 'll') and self.origin in (None, 'sw', 'll'):
+            return True
+        if origin in (None, 'nw', 'ul') and self.origin in (None, 'nw', 'ul'):
             return True
         grid_size = self.grid_sizes[0]
         level_0_bbox = self._tiles_bbox([(0, 0, 0),

mapproxy/service/wmts.py
@@ -192,8 +192,12 @@ class TileMatrixSet(object):
         self.grid = grid
         self.name = grid.name
         self.srs_name = grid.srs.srs_code
+        self.tile_matrices = list(self._tile_matrices())
 
     def __iter__(self):
+        return iter(self.tile_matrices)
+
+    def _tile_matrices(self):
         for level, res in self.grid.resolutions.iteritems():
             origin = self.grid.origin_tile(level, 'ul')
             bbox = self.grid.tile_bbox(origin)

mapproxy/test/unit/test_grid.py
@@ -698,6 +698,15 @@ class TestOrigins(object):
         assert grid.supports_access_with_origin('ll')
         assert grid.supports_access_with_origin('ul')
 
+    def test_basic_mixed_name(self):
+        grid = tile_grid(4326, bbox=(-180, -90, 180, 90), origin='ll')
+        assert grid.supports_access_with_origin('sw')
+        assert not grid.supports_access_with_origin('nw')
+
+        grid = tile_grid(4326, bbox=(-180, -90, 180, 90), origin='ul')
+        assert not grid.supports_access_with_origin('sw')
+        assert grid.supports_access_with_origin('nw')
+
     def test_custom_with_match(self):
         # height is divisible by res*tile_size
         grid = tile_grid(4326, bbox=(0, 0, 1024, 1024), origin='ll',

commit c6daaba10c564d9c4d2fd466fb8b0fa6ebe1ce3c

fixed bug in tile origin compatibility calculations (introduced with 7e58eee)

mapproxy/grid.py
@@ -430,8 +430,9 @@ class TileGrid(object):
     def supports_access_with_origin(self, origin):
         if origin in (None, 'sw', 'll') and self.origin in (None, 'sw', 'll'):
             return True
-        if origin in (None, 'nw', 'ul') and self.origin in (None, 'nw', 'ul'):
+        if origin in ('nw', 'ul') and self.origin in ('nw', 'ul'):
             return True
+
         grid_size = self.grid_sizes[0]
         level_0_bbox = self._tiles_bbox([(0, 0, 0),
             (grid_size[0] - 1, grid_size[1] - 1, 0)])