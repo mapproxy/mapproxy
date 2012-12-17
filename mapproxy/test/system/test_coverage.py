b9deb2092fb8b0676511f58590b317dabd74d345
Oliver Tonnhofer <olt@bogosoft.com>
2012-12-17 08:15:06 +0100

    fix rounding errors in bbox_position_in_image
    use integers when calculating offsets (will be converted to int
    later anyways) to get the size calculation right.
    
    closes #48

--- mapproxy/test/system/test_coverage.py
+++ mapproxy/test/system/test_coverage.py
@@ -1,12 +1,12 @@
 # This file is part of the MapProxy project.
 # Copyright (C) 2010 Omniscale <http://omniscale.de>
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,11 +35,11 @@ class TestCoverageWMS(SystemTest):
     config = test_config
     def setup(self):
         SystemTest.setup(self)
-        self.common_map_req = WMS111MapRequest(url='/service?', param=dict(service='WMS', 
+        self.common_map_req = WMS111MapRequest(url='/service?', param=dict(service='WMS',
              version='1.1.1', bbox='-180,0,0,80', width='200', height='200',
              layers='wms_cache', srs='EPSG:4326', format='image/png',
              styles='', request='GetMap'))
-    
+
     def test_capababilities(self):
         resp = self.app.get('/service?request=GetCapabilities&service=WMS&version=1.1.1')
         xml = resp.lxml
@@ -59,7 +59,7 @@ class TestCoverageWMS(SystemTest):
         img = Image.open(data)
         eq_(img.mode, 'RGB')
         eq_(img.getcolors(), [(200*200, (255, 0, 5))])
-    
+
     def test_get_map_outside_transparent(self):
         self.common_map_req.params.bbox = -90, 0, 0, 90
         self.common_map_req.params.transparent = True
@@ -71,14 +71,14 @@ class TestCoverageWMS(SystemTest):
         eq_(img.mode, 'RGBA')
         eq_(img.getcolors()[0][0], 200*200)
         eq_(img.getcolors()[0][1][3], 0) # transparent
-    
+
     def test_get_map_intersection(self):
         self.created_tiles.append('wms_cache_EPSG4326/03/000/000/004/000/000/002.jpeg')
         with tmp_image((256, 256), format='jpeg') as img:
             expected_req = ({'path': r'/service?LAYERs=foo,bar&SERVICE=WMS&FORMAT=image%2Fjpeg'
                                       '&REQUEST=GetMap&HEIGHT=91&SRS=EPSG%3A4326&styles='
                                       '&VERSION=1.1.1&BBOX=10,15,30,31'
-                                      '&WIDTH=113'},
+                                      '&WIDTH=114'},
                             {'body': img.read(), 'headers': {'content-type': 'image/jpeg'}})
             with mock_httpd(('localhost', 42423), [expected_req]):
                 self.common_map_req.params.bbox = 0, 0, 40, 40
@@ -91,11 +91,11 @@ class TestCoverageWMS(SystemTest):
 
 class TestCoverageTMS(SystemTest):
     config = test_config
-    
+
     def test_get_tile_intersections(self):
         with tmp_image((256, 256), format='jpeg') as img:
             expected_req = ({'path': r'/service?LAYERs=foo,bar&SERVICE=WMS&FORMAT=image%2Fjpeg'
-                                      '&REQUEST=GetMap&HEIGHT=24&SRS=EPSG%3A900913&styles='
+                                      '&REQUEST=GetMap&HEIGHT=25&SRS=EPSG%3A900913&styles='
                                       '&VERSION=1.1.1&BBOX=1113194.90793,1689200.13961,3339584.7238,3632749.14338'
                                       '&WIDTH=28'},
                             {'body': img.read(), 'headers': {'content-type': 'image/jpeg'}})
@@ -103,7 +103,7 @@ class TestCoverageTMS(SystemTest):
                 resp = self.app.get('/tms/1.0.0/wms_cache/0/1/1.jpeg')
                 eq_(resp.content_type, 'image/jpeg')
                 self.created_tiles.append('wms_cache_EPSG900913/01/000/000/001/000/000/001.jpeg')
-    
+
     def test_get_tile_intersection_tms(self):
         with tmp_image((256, 256), format='jpeg') as img:
             expected_req = ({'path': r'/tms/1.0.0/foo/1/1/1.jpeg'},
