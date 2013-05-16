440a560b627b1ad21057210e333646cd5c12b2ca
Marcel Radischat <radischat@omniscale.de>
2013-05-16 09:02:52 +0200

    added explicit version option

--- mapproxy/script/util.py
+++ mapproxy/script/util.py
@@ -316,13 +316,17 @@ def print_items(data, title='Commands'):
 
 def main():
     parser = NonStrictOptionParser("usage: %prog COMMAND [options]",
-        version='MapProxy ' + version, add_help_option=False)
+        add_help_option=False)
     options, args = parser.parse_args()
 
     if len(args) < 1 or args[0] in ('--help', '-h'):
         parser.print_help()
         print
         print_items(commands)
+        sys.exit(1)
+
+    if len(args) == 1 and args[0] == '--version':
+        print 'Mapproxy ' + version
         sys.exit(1)
 
     command = args[0]
