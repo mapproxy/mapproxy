d882582c7a0d8233e735333e61ac128157ad7431
Oliver Tonnhofer <olt@bogosoft.com>
2013-11-23 17:11:20 +0100

    fix raise exception

--- mapproxy/service/wms.py
+++ mapproxy/service/wms.py
@@ -512,7 +512,7 @@ class LayerRenderer(object):
                     errors.append(ex[1].args[0])
                 else:
                     async_pool.shutdown(True)
-                    raise ex[0], ex[1], ex[2]
+                    raise ex[1]
 
         if render_layers and not rendered:
             errors = '\n'.join(errors)
