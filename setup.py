bd2c62201b44776a5b2215b6deab48ba7a2dc84e
Oliver Tonnhofer <olt@bogosoft.com>
2014-07-08 14:21:04 +0200

    bump version

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def long_description(changelog_releases=10):
 
 setup(
     name='MapProxy',
-    version="1.7.0a",
+    version="1.8.0a",
     description='An accelerating proxy for web map services',
     long_description=long_description(7),
     author='Oliver Tonnhofer',
