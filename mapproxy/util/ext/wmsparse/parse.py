d73dd51f90c838a56bb1020aa2d71e646044c7aa
Steven D. Lander <steven.lander@rgi-corp.com>
2016-03-16 12:43:28 -0400

    fixing for test case

--- mapproxy/util/ext/wmsparse/parse.py
+++ mapproxy/util/ext/wmsparse/parse.py
@@ -53,7 +53,7 @@ class WMSCapabilities(object):
 
     def parse_contact(self):
         elem = self.find(self.tree, 'Service/ContactInformation')
-        if elem is not None:
+        if elem is None or len(elem) == 0:
             elem = etree.Element(None)
         md = dict(
             person = self.findtext(elem, 'ContactPersonPrimary/ContactPerson'),
