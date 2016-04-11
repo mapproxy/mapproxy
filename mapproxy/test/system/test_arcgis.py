2b5c103c286763cff873b1afbe42b197faaf775a
Ian Dees <ian.dees@gmail.com>
2016-04-10 21:51:59 -0400

    Add a system test

---
+++ mapproxy/test/system/test_arcgis.py
@@ -0,0 +1,60 @@
+# This file is part of the MapProxy project.
+# Copyright (C) 2010 Omniscale <http://omniscale.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import with_statement, division
+
+from io import BytesIO
+from mapproxy.request.arcgis import ArcGISRequest
+from mapproxy.test.image import is_png, create_tmp_image
+from mapproxy.test.http import mock_httpd
+from mapproxy.test.system import module_setup, module_teardown, SystemTest
+from nose.tools import eq_
+
+test_config = {}
+
+def setup_module():
+    module_setup(test_config, 'arcgis.yaml')
+
+def teardown_module():
+    module_teardown(test_config)
+
+transp = create_tmp_image((512, 512), mode='RGBA', color=(0, 0, 0, 0))
+
+class TestArcgisSource(SystemTest):
+    config = test_config
+    def setup(self):
+        SystemTest.setup(self)
+        self.common_map_req = ArcGISRequest(url='/arcgis/rest/services/ExampleLayer/ImageServer')
+
+    def test_get_tile(self):
+        expected_req = [({'path': '/arcgis/rest/services/ExampleLayer/ImageServer/export?f=image&format=png&imageSR=900913&bboxSR=900913&bbox=-20037508.3428,-20037508.3428,20037508.3428,20037508.3428&size=512,512'},
+                 {'body': transp, 'headers': {'content-type': 'image/png'}}),
+                ]
+
+        with mock_httpd(('localhost', 42423), expected_req):
+            resp = self.app.get('/tms/1.0.0/app2_layer/0/0/1.png')
+            eq_(resp.content_type, 'image/png')
+            eq_(resp.content_length, len(resp.body))
+            data = BytesIO(resp.body)
+            assert is_png(data)
+
+    def test_get_tile_from_missing_arcgis_layer(self):
+        expected_req = [({'path': '/arcgis/rest/services/ExampleLayer/ImageServer/export?f=image&format=png&imageSR=900913&bboxSR=900913&bbox=-20037508.3428,-20037508.3428,20037508.3428,20037508.3428&size=512,512'},
+                 {'body': b'', 'status': 500}),
+                ]
+
+        with mock_httpd(('localhost', 42423), expected_req):
+            resp = self.app.get('/tms/1.0.0/app2_layer/0/0/1.png')
+            eq_(resp.status, 500)
