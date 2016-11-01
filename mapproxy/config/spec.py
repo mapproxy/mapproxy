e6531b7d617e148fedaffdffc00347289d41e231
Damiano Albani <damiano.albani@gmail.com>
2016-11-01 08:51:01 +0100

    Support "tile_lock_dir" configuration property for S3 cache

--- mapproxy/config/spec.py
+++ mapproxy/config/spec.py
@@ -136,6 +136,7 @@ cache_types = {
         'bucket_name': str(),
         'directory_layout': str(),
         'directory': str(),
+        'tile_lock_dir': str(),
         'profile_name': str(),
      },
     'riak': {
