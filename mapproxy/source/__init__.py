mapproxy/mapproxy

commit 5e3005fe9f12868f04d84554491aa484d52e2b21

fix WMS queries for multiple non-transparent, but clipped layers
Transparent property of layers/sources was used to optimize WMS requests for
multiple layers. Layers below an opaque layer would not be queried. However,
with min_res/max_res and coverages a `transparent:false` source can still return
a transparent image.

This commit removes transparent property from layer.
is_opaque is now used to determine if the layer/source is definitely opaque.

mapproxy/source/__init__.py
@@ -45,8 +45,8 @@ class DebugSource(MapLayer):
     def __init__(self):
         MapLayer.__init__(self)
         self.extent = DefaultMapExtent()
-        self.transparent = True
         self.res_range = None
+
     def get_map(self, query):
         bbox = query.bbox
         w = bbox[2] - bbox[0]
@@ -68,7 +68,7 @@ class DummySource(MapLayer):
     def __init__(self, coverage=None):
         MapLayer.__init__(self)
         self.extent = MapExtent((-180, -90, 180, 90), SRS(4326))
-        self.transparent = True
         self.extent = MapExtent(coverage.bbox, coverage.srs) if coverage else DefaultMapExtent()
+
     def get_map(self, query):
         raise BlankImage()

mapproxy/cache/tile.py
@@ -72,7 +72,6 @@ class TileManager(object):
         self.sources = sources
         self.minimize_meta_requests = minimize_meta_requests
         self._expire_timestamp = None
-        self.transparent = self.sources[0].transparent
         self.pre_store_filter = pre_store_filter or []
         self.concurrent_tile_creators = concurrent_tile_creators
         self.tile_creator_class = tile_creator_class or TileCreator

mapproxy/config/loader.py
@@ -1528,7 +1528,7 @@ class CacheConfiguration(ConfigurationBase):
         if len(caches) == 1:
             layer = caches[0][0]
         else:
-            layer = SRSConditional(caches, caches[0][0].extent, caches[0][0].transparent, opacity=image_opts.opacity)
+            layer = SRSConditional(caches, caches[0][0].extent, opacity=image_opts.opacity)
 
         if 'use_direct_from_level' in self.conf:
             self.conf['use_direct_from_res'] = main_grid.resolution(self.conf['use_direct_from_level'])

mapproxy/layer.py
@@ -50,14 +50,6 @@ class MapLayer(object):
     def __init__(self, image_opts=None):
         self.image_opts = image_opts or ImageOptions()
 
-    def _get_transparent(self):
-        return self.image_opts.transparent
-
-    def _set_transparent(self, value):
-        self.image_opts.transparent = value
-
-    transparent = property(_get_transparent, _set_transparent)
-
     def _get_opacity(self):
         return self.image_opts.opacity
 
@@ -66,10 +58,17 @@ class MapLayer(object):
 
     opacity = property(_get_opacity, _set_opacity)
 
-    def is_opaque(self):
-        if self.opacity is None:
-            return not self.transparent
-        return self.opacity >= 0.99
+    def is_opaque(self, query):
+        """
+        Whether the query result is opaque.
+
+        This method is used for optimizations: layers below an opaque
+        layer can be skipped. As sources with `transparent: false`
+        still can return transparent images (min_res/max_res/coverages),
+        implementations of this method need to be certain that the image
+        is indeed opaque. is_opaque should return False if in doubt.
+        """
+        return False
 
     def check_res_range(self, query):
         if (self.res_range and
@@ -305,8 +304,6 @@ class ResolutionConditional(MapLayer):
         self.resolution = resolution
         self.srs = srs
 
-        #TODO
-        self.transparent = self.one.transparent
         self.opacity = opacity
         self.extent = extent
 
@@ -331,9 +328,8 @@ class SRSConditional(MapLayer):
     PROJECTED = 'PROJECTED'
     GEOGRAPHIC = 'GEOGRAPHIC'
 
-    def __init__(self, layers, extent, transparent=False, opacity=None):
+    def __init__(self, layers, extent, opacity=None):
         MapLayer.__init__(self)
-        self.transparent = transparent
         # TODO geographic/projected fallback
         self.srs_map = {}
         self.res_range = merge_layer_res_ranges([l[0] for l in layers])
@@ -403,7 +399,6 @@ class CacheMapLayer(MapLayer):
         self.grid = tile_manager.grid
         self.extent = extent or map_extent_from_grid(self.grid)
         self.res_range = merge_layer_res_ranges(self.tile_manager.sources)
-        self.transparent = tile_manager.transparent
         self.max_tile_limit = max_tile_limit
 
     def get_map(self, query):

mapproxy/service/wms.py
@@ -108,7 +108,7 @@ class WMSServer(Server):
             if layer.renders_query(query):
                 # if layer is not transparent and will be rendered,
                 # remove already added (then hidden) layers
-                if not layer.transparent:
+                if layer.is_opaque(query):
                     actual_layers = odict()
                 for layer_name, map_layers in layer.map_layers_for_query(query):
                     actual_layers[layer_name] = map_layers
@@ -621,8 +621,6 @@ class WMSLayerBase(object):
     "True if .info() is supported"
     queryable = False
 
-    transparent = False
-
     "True is .legend() is supported"
     has_legend = False
     legend_url = None
@@ -632,9 +630,6 @@ class WMSLayerBase(object):
     res_range = None
     "MapExtend of the layer"
     extent = None
-
-    def is_opaque(self):
-        return not self.transparent
 
     def map_layers_for_query(self, query):
         raise NotImplementedError()
@@ -666,8 +661,10 @@ class WMSLayer(WMSLayerBase):
             res_range = merge_layer_res_ranges(map_layers)
         self.res_range = res_range
         self.queryable = True if info_layers else False
-        self.transparent = all(not map_lyr.is_opaque() for map_lyr in self.map_layers)
         self.has_legend = True if legend_layers else False
+
+    def is_opaque(self, query):
+        return any(l.is_opaque(query) for l in self.map_layers)
 
     def renders_query(self, query):
         if self.res_range and not self.res_range.contains(query.bbox, query.size, query.srs):
@@ -727,12 +724,14 @@ class WMSGroupLayer(WMSLayerBase):
         self.md = md or {}
         self.is_active = True if this is not None else False
         self.layers = layers
-        self.transparent = True if this and not this.is_opaque() or all(not l.is_opaque() for l in layers) else False
         self.has_legend = True if this and this.has_legend or any(l.has_legend for l in layers) else False
         self.queryable = True if this and this.queryable or any(l.queryable for l in layers) else False
         all_layers = layers + ([self.this] if self.this else [])
         self.extent = merge_layer_extents(all_layers)
         self.res_range = merge_layer_res_ranges(all_layers)
+
+    def is_opaque(self, query):
+        return any(l.is_opaque(query) for l in self.layers)
 
     @property
     def legend_size(self):

mapproxy/source/mapnik.py
@@ -148,4 +148,4 @@ class MapnikSource(MapLayer):
                 status='200' if data else '500', size=size, method='API', duration=time.time()-start_time)
 
         return ImageSource(BytesIO(data), size=query.size,
-            image_opts=ImageOptions(transparent=self.transparent, format=query.format))
+            image_opts=ImageOptions(format=query.format))

mapproxy/source/wms.py
@@ -44,13 +44,37 @@ class WMSSource(MapLayer):
         self.transparent_color = transparent_color
         self.transparent_color_tolerance = transparent_color_tolerance
         if self.transparent_color:
-            self.transparent = True
+            self.image_opts.transparent = True
         self.coverage = coverage
         self.res_range = res_range
         if self.coverage:
             self.extent = MapExtent(self.coverage.bbox, self.coverage.srs)
         else:
             self.extent = DefaultMapExtent()
+
+    def is_opaque(self, query):
+        """
+        Returns true if we are sure that the image is not transparent.
+        """
+        if self.res_range and not self.res_range.contains(query.bbox, query.size,
+                                                          query.srs):
+            return False
+
+        if self.image_opts.transparent:
+            return False
+
+        if self.opacity is not None and (0.0 < self.opacity < 0.99):
+            return False
+
+        if not self.coverage:
+            # not transparent and no coverage
+            return True
+
+        if self.coverage.contains(query.bbox, query.srs):
+            # not transparent and completely inside coverage
+            return True
+
+        return False
 
     def get_map(self, query):
         if self.res_range and not self.res_range.contains(query.bbox, query.size,

mapproxy/test/unit/test_cache.py
@@ -869,7 +869,6 @@ class MockLayer(object):
 class TestResolutionConditionalLayers(object):
     def setup(self):
         self.low = MockLayer()
-        self.low.transparent = False #TODO
         self.high = MockLayer()
         self.layer = ResolutionConditional(self.low, self.high, 10, SRS(900913),
             GLOBAL_GEOGRAPHIC_EXTENT)

commit 49dd692e3e6bb05d1cf7e847d838c1a789e0d113

fix transparency for dummy source
regression from 5e3005f

mapproxy/source/__init__.py
@@ -67,6 +67,7 @@ class DummySource(MapLayer):
     """
     def __init__(self, coverage=None):
         MapLayer.__init__(self)
+        self.image_opts.transparent = True
         self.extent = MapExtent((-180, -90, 180, 90), SRS(4326))
         self.extent = MapExtent(coverage.bbox, coverage.srs) if coverage else DefaultMapExtent()
 