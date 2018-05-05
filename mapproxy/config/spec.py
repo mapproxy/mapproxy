0c4126198ce88147ea1020b9828d836d49054c54
mikkokut <mikko93@gmail.com>
2018-05-05 11:07:47 +0300

    add region_name and endpoint_url to global section spec

--- mapproxy/config/spec.py
+++ mapproxy/config/spec.py
@@ -371,6 +371,8 @@ mapproxy_yaml_spec = {
             's3': {
                 'bucket_name': str(),
                 'profile_name': str(),
+                'region_name': str(),
+                'endpoint_url': str(),
             },
         },
         'grid': {
