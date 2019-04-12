c336a81d852582d759a3997e895fcafe4c31744b
Arne Babenhauserheide <arne_bab@web.de>
2019-04-12 15:38:30 +0200

    clearer comments and using the lock again

--- mapproxy/source/mapnik.py
+++ mapproxy/source/mapnik.py
@@ -59,9 +59,11 @@ class MapnikSource(MapLayer):
         self.layers = set(layers) if layers else None
         self.scale_factor = scale_factor
         self.lock = lock
-        # global objects to support multiprocessing
+        # global objects allow caching over multiple instances within the same worker process
         global _map_objs
         _map_objs = {}
+        global _map_objs_lock
+        _map_objs_lock = threading.Lock()
         self._cache_map_obj = reuse_map_objects
         if self.coverage:
             self.extent = MapExtent(self.coverage.bbox, self.coverage.srs)
@@ -108,30 +110,38 @@ class MapnikSource(MapLayer):
         #  file handles that gets passed to the child)
         # segment the cache by process and thread to avoid interference
         if self._cache_map_obj:
-            cachekey = None # renderd guarantees that there are no concurrency issues
+            # all MapnikSources with the same mapfile share the same Mapnik Map.
+            cachekey = (None, None, mapfile)
         else:
             thread_id = threading.current_thread().ident
             process_id = multiprocessing.current_process()._identity
             cachekey = (process_id, thread_id, mapfile)
-        if cachekey not in _map_objs:
-            _map_objs[cachekey] = self._create_map_obj(mapfile)
+        with _map_objs_lock:
+            if cachekey not in _map_objs:
+                _map_objs[cachekey] = self._create_map_obj(mapfile)
+            mapnik_map = _map_objs[cachekey]
 
         # clean up no longer used cached maps
         process_cache_keys = [k for k in _map_objs.keys()
                               if k[0] == process_id]
+        # To avoid time-consuming cleanup whenever one thread in the
+        # threadpool finishes, allow ignoring up to 5 dead mapnik
+        # instances.  (5 is empirical)
         if len(process_cache_keys) > (5 + threading.active_count()):
             active_thread_ids = set(i.ident for i in threading.enumerate())
             for k in process_cache_keys:
-                if not k[1] in active_thread_ids and k in _map_objs:
-                    try:
-                        m = _map_objs[k]
-                        del _map_objs[k]
-                    except KeyError:
-                        continue
-                    m.remove_all() # cleanup
-                    mapnik.clear_cache()
+                with _map_objs_lock:
+                    if not k[1] in active_thread_ids and k in _map_objs:
+                        try:
+                            m = _map_objs[k]
+                            del _map_objs[k]
+                        except KeyError:
+                            continue
+                        # cleanup
+                        m.remove_all()
+                        mapnik.clear_cache()
 
-        return _map_objs[cachekey]
+        return mapnik_map
 
     def render_mapfile(self, mapfile, query):
         return run_non_blocking(self._render_mapfile, (mapfile, query))
