83cd3c342c3d7bdb8e1476e2cc0b9e439fffe37f
Stijn Blommerde <stijn@apptimize.nl>
2023-01-13 17:32:18 +0100

    remove formatting

--- mapproxy/image/merge.py
+++ mapproxy/image/merge.py
@@ -25,15 +25,12 @@ from mapproxy.image.opts import create_image, ImageOptions
 from mapproxy.image.mask import mask_image
 
 import logging
-
 log = logging.getLogger('mapproxy.image')
-
 
 class LayerMerger(object):
     """
     Merge multiple layers into one image.
     """
-
     def __init__(self):
         self.layers = []
         self.cacheable = True
@@ -62,9 +59,9 @@ class LayerMerger(LayerMerger):
             layer_img, layer_coverage = self.layers[0]
             layer_opts = layer_img.image_opts
             if (((layer_opts and not layer_opts.transparent) or image_opts.transparent)
-                    and (not size or size == layer_img.size)
-                    and (not layer_coverage or not layer_coverage.clip)
-                    and not coverage):
+                and (not size or size == layer_img.size)
+                and (not layer_coverage or not layer_coverage.clip)
+                and not coverage):
                 # layer is opaque, no need to make transparent or add bgcolor
                 return layer_img
 
@@ -138,7 +135,6 @@ class LayerMerger(LayerMerger):
 
 band_ops = namedtuple("band_ops", ["dst_band", "src_img", "src_band", "factor"])
 
-
 class BandMerger(object):
     """
     Merge bands from multiple sources into one image.
@@ -155,7 +151,6 @@ class BandMerger(object):
                {source: dop_cache, band: 2, factor: 0.1},
            ]
     """
-
     def __init__(self, mode=None):
         self.ops = []
         self.cacheable = True
@@ -169,10 +164,10 @@ class BandMerger(object):
             src_img=src_img,
             src_band=src_band,
             factor=factor,
-        ))
+         ))
         # store highest requested band index for each source
         self.max_band[src_img] = max(self.max_band.get(src_img, 0), src_band)
-        self.max_src_images = max(src_img + 1, self.max_src_images)
+        self.max_src_images = max(src_img+1, self.max_src_images)
 
     def merge(self, sources, image_opts, size=None, bbox=None, bbox_srs=None, coverage=None):
         if len(sources) < self.max_src_images:
@@ -273,7 +268,7 @@ def concat_legends(legends, format='png', size=None, bgcolor='#ffffff', transpar
     :rtype: `ImageSource`
     """
     if not legends:
-        return BlankImageSource(size=(1, 1), image_opts=ImageOptions(bgcolor=bgcolor, transparent=transparent))
+        return BlankImageSource(size=(1,1), image_opts=ImageOptions(bgcolor=bgcolor, transparent=transparent))
     if len(legends) == 1:
         return legends[0]
 
@@ -283,12 +278,12 @@ def concat_legends(legends, format='png', size=None, bgcolor='#ffffff', transpar
         legend_width = 0
         legend_height = 0
         legend_position_y = []
-        # iterate through all legends, last to first, calc img size and remember the y-position
+        #iterate through all legends, last to first, calc img size and remember the y-position
         for legend in legends:
             legend_position_y.append(legend_height)
             tmp_img = legend.as_image()
             legend_width = max(legend_width, tmp_img.size[0])
-            legend_height += tmp_img.size[1]  # images shall not overlap themselfs
+            legend_height += tmp_img.size[1] #images shall not overlap themselfs
 
         size = [legend_width, legend_height]
     bgcolor = ImageColor.getrgb(bgcolor)
@@ -313,10 +308,10 @@ def concat_json_legends(legends):
     :param legends: list of HttpResponse objects
     :rtype json formatted string
     """
-    legendsDict = {'Legend': []}
+    legends_dict = {'Legend': []}
     legends = legends[:]
     for legend in legends:
-        legendStr = legend.read().decode('utf-8')
-        legendDict = json.loads(legendStr)
-        legendsDict['Legend'].append(legendDict['Legend'][0])
-    return json.dumps(legendsDict)
+        legend_str = legend.read().decode('utf-8')
+        legend_dict = json.loads(legend_str)
+        legends_dict['Legend'].append(legend_dict['Legend'][0])
+    return json.dumps(legends_dict)
