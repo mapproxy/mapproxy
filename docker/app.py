0a2f8d911d2c9bafd54be185046f154a2a3055cf
Johannes Weskamm <weskamm@terrestris.de>
2023-03-29 15:24:36 +0200

    Introduce docker
    Co-authored-by: Jan Suleiman <jansule@users.noreply.github.com>
    Co-authored-by: Jakob Miksch <info@jakobmiksch.eu>

---
+++ docker/app.py
@@ -0,0 +1,10 @@
+# WSGI module for use with Apache mod_wsgi or gunicorn
+
+# # uncomment the following lines for logging
+# # create a log.ini with `mapproxy-util create -t log-ini`
+# from logging.config import fileConfig
+# import os.path
+# fileConfig(r'/mapproxy/config/log.ini', {'here': os.path.dirname(__file__)})
+
+from mapproxy.wsgiapp import make_wsgi_app
+application = make_wsgi_app(r'/mapproxy/config/mapproxy.yaml', reloader=True)
