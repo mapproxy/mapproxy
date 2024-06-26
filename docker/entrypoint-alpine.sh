aba13df91ece2cacc18c5a0ea7ca1e36fe514dce
Simon Seyock <seyock@terrestris.de>
2024-06-26 09:10:16 +0200

    fix: fix ubuntu dockerfile

--- docker/entrypoint-alpine.sh
+++ docker/entrypoint-alpine.sh
@@ -2,15 +2,10 @@
 
 cd /mapproxy
 
-addgroup -S mapproxy && \
-  adduser -D -s /bin/sh -S -h /mapproxy/ -G mapproxy mapproxy && \
-  mkdir -p /var/cache/nginx/ /mapproxy/config/cache_data/ && \
-  chown -R mapproxy:mapproxy /mapproxy/config/ /var/cache/nginx/
-
 # create config files if they do not exist yet
 if [ ! -f /mapproxy/config/mapproxy.yaml ]; then
   echo "No mapproxy configuration found. Creating one from template."
   mapproxy-util create -t base-config config
 fi
 
-su mapproxy -c "$@"
+exec "$@"
