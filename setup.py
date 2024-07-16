846682cc187a14ed5edec18f77111a9243923961
Johannes Weskamm <weskamm@terrestris.de>
2024-07-16 11:32:39 +0200

    dev: prepare 2.1.0 release

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ def long_description(changelog_releases=10):
 
 setup(
     name='MapProxy',
-    version="2.0.2",
+    version="2.1.0",
     description='An accelerating proxy for tile and web map services',
     long_description=long_description(7),
     author='Oliver Tonnhofer',
