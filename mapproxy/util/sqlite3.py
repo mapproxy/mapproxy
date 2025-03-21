7195684b7556a8667cf39f1529a9212337c1bc26
Simon Seyock <seyock@terrestris.de>
2025-03-21 17:08:27 +0100

    chore: linting

--- mapproxy/util/sqlite3.py
+++ mapproxy/util/sqlite3.py
@@ -6,13 +6,16 @@ def adapt_date_iso(val):
     """Adapt datetime.date to ISO 8601 date."""
     return val.isoformat()
 
+
 def adapt_datetime_iso(val):
     """Adapt datetime.datetime to timezone-naive ISO 8601 date."""
     return val.isoformat()
 
+
 def convert_date(val):
     """Convert ISO 8601 date to datetime.date object."""
     return datetime.date.fromisoformat(val)
+
 
 def convert_datetime(val):
     """Convert ISO 8601 datetime to datetime.datetime object."""
