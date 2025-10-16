2c8e6fcff92e46a5f0c871f8f8f9e3ac51b9d7d0
Even Rouault <even.rouault@spatialys.com>
2025-10-16 14:59:13 +0200

    spec.py: add concurrent_layer_renderer

--- mapproxy/config/spec.py
+++ mapproxy/config/spec.py
@@ -510,6 +510,7 @@ mapproxy_yaml_spec = {
             },
             'on_source_errors': str(),
             'max_output_pixels': one_of(number(), [number()]),
+            'concurrent_layer_renderer': number(),
             'md': {
                 'identification': {
                     'title': str(),
