9f6c0c9e59abda9e1cccda62a3fe899edfd4dcc5
Andre Henn <henn@terrestris.de>
2026-04-30 15:27:13 +0200

    fix: path of default configuration in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,7 @@ cd /mapproxy
 # create config files if they do not exist yet
 if [ ! -f /mapproxy/config/mapproxy.yaml ] && [ "$MULTIAPP_MAPPROXY" != "true" ]; then
   echo "No mapproxy configuration found. Creating one from template."
-  mapproxy-util create -t base-config config
+  mapproxy-util create -t base-config /mapproxy/config/
 fi
 
 exec "$@"
