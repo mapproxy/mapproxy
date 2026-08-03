fc88e2e09f4e2aa2480db22c5583c789de9b01f7
charlien <charlien@met.no>
2026-08-03 12:52:15 +0200

    add test

---
+++ mapproxy/test/system/test_cache_mbtiles_dimensions.py
@@ -0,0 +1,71 @@
+# This file is part of the MapProxy project.
+# Copyright (C) 2011 Omniscale <http://omniscale.de>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import division
+
+import glob
+import os
+import shutil
+
+from io import BytesIO
+
+from mapproxy.request.wms import WMS111MapRequest
+from mapproxy.test.http import MockServ
+from mapproxy.test.image import is_png, create_tmp_image
+from mapproxy.test.system import SysTest
+
+import pytest
+
+
+@pytest.fixture(scope="module")
+def config_file():
+    return "cache_mbtiles_dimensions.yaml"
+
+
+@pytest.fixture(scope="class")
+def fixture_gpkg(base_dir):
+    shutil.copy(
+        os.path.join(os.path.dirname(__file__), "fixture",
+                     "cache.2026-08-03T06:00:00.000Z.mbtiles"),
+        base_dir.strpath,
+    )
+
+
+@pytest.mark.usefixtures("fixture_gpkg")
+class TestMBTilesDimensionsCache(SysTest):
+
+    def setup_method(self):
+        self.common_map_req = WMS111MapRequest(
+            url="/service?",
+            param=dict(
+                service="WMS",
+                version="1.1.1",
+                bbox="-180,-80,0,0",
+                width="200",
+                height="200",
+                layers="mb",
+                srs="EPSG:4326",
+                format="image/png",
+                styles="",
+                request="GetMap",
+                time="2026-08-03T06:00:00.000Z"
+            ),
+        )
+
+    def test_get_map_cached(self, app):
+        resp = app.get(self.common_map_req)
+        assert resp.content_type == "image/png"
+        data = BytesIO(resp.body)
+        assert is_png(data)
